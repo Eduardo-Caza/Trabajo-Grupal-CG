c767695e7abf62ec96188896d07cb57821c2cabe
Eduardo-Caza <ecaanaranjo@gmail.com>
2024-11-12 21:25:29 -0500

    no estaba terminado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+		apiKey: 'AIzaSyCOAiqiQFM9HFh9kTyMB9OQi7XszK',
+		authDomain: 'proyectoejm-8c510.firebaseapp.com',
+		projectId: "proyectoejm-8c510",
+    storageBucket: "proyectoejm-8c510.firebasestorage.app",
+    messagingSenderId: "856456070152",
+    appId: "1:856456070152:web:6fe9d8537768c2c9a87dbd"
+	},
 };
